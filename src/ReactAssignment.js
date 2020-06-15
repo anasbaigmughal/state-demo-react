8505bf48cc5415b6eca66140ed9c5ff2c566960b
Muhammad Anas Baig <anasbaigmughal@gmail.com>
2020-06-16 00:18:09 +0500

    Update ReactAssignment.js

--- src/ReactAssignment.js
+++ src/ReactAssignment.js
@@ -9,7 +9,11 @@ function ReactAssignment(props){
     return <div>
       <strong><u>Task1:</u> Add 2 more buttons: “ON” and “OFF”. Turn the light either ON or OFF depending on which button is clicked by wiring up the onClick handlers to set the state.</strong>
       <br/>
-      <strong><u>Solution:</u></strong>
+      <strong><u>Task2:</u> Add another piece of state: the room temperature. (Hint: you can call useState more than once!) Initialize it to 72 (or 22 for you Celsius types). Display the current temperature under the light status.</strong>
+      <br/>
+      <strong><u>Task3:</u> Add 2 more buttons: “+” and “-”. Then add onClick handlers that will increase or decrease the temperature by 1 degree when clicked.</strong>
+      <br/>
+      <strong><u>Solution1, 2, 3:</u></strong>
       <div className= {`room ${isLit?"lit": "dark"}`}>
       <br/>
       <strong>Room 2:</strong>
